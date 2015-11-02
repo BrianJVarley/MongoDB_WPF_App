b6d0b7972fcb760c7ea41b7cf7b4f927a8d31b2d
BrianJVarley <brianvarley9@gmail.com>
2015-11-02 22:15:42 +0000

    Added MongoDB .Net dll and set up DB connection.

--- MongoDBApp/MainWindow.xaml.cs
+++ MongoDBApp/MainWindow.xaml.cs
@@ -12,17 +12,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace MongoDBApp
 {
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _client = new MongoClient();
+            _database = _client.GetDatabase("local");
+
+
         }
     }
 }
