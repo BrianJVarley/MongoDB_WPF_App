d4c0de0eae47e0c3b0c14bf6fc1dbfbb6911e6e3
BrianJVarley <brianvarley9@gmail.com>
2015-11-04 17:34:42 +0000

    Added 'Customer' POCO to Models folder

---
+++ MongoDBApp/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBApp.Models
+{
+    public class Customer
+    {
+        /// <summary>
+        /// This attribute is used to map the Id property to the ObjectId in the collection
+        /// </summary>
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+      
+        [BsonElement("fisrtName")]
+        public string firstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string lastName { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+    }
+}
