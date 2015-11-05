fc7acbc63031d69fe2c92c48b513b4355b202770
BrianJVarley <brianvarley9@gmail.com>
2015-11-05 23:27:43 +0000

    Added MongoDB connection as per driver v2.1.0. //Notes: 1.Add regex/validation to email field on UI 2.Extract database query and connection code to relevant models

--- MongoDBApp/Views/MainWindow.xaml.cs
+++ MongoDBApp/Views/MainWindow.xaml.cs
@@ -14,26 +14,28 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Shared;
 
 namespace MongoDBApp
 {
-    using MongoDB.Bson;
-    using MongoDB.Driver;
-   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        protected static IMongoClient _client;
-        protected static IMongoDatabase _database;
-
+        private string connectionString = "mongodb://<brianVarley>:<Starlight123;>@ds048878.mongolab.com:48878/orders";
+        
         public MainWindow()
         {
             InitializeComponent();
 
-            _client = new MongoClient();
-            _database = _client.GetDatabase("local");
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("orders");
+
+            //Get a handle on the customers collection:
+            var collection = database.GetCollection<BsonDocument>("customers");
+            //var documents = await collection.Find(new BsonDocument()).ToListAsync();
 
 
         }
