22332a3f7ef838b2bbdc3dee9aa6a3102e8f523b
BrianJVarley <brianvarley9@gmail.com>
2015-11-18 12:53:48 +0000

    Added CollectionExtension class for ObservableCollection return in MainVM

---
+++ MongoDBApp/Extensions/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBApp.Extensions
+{
+    static class CollectionExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> enumerableResult)
+        {
+            return new ObservableCollection<T>(enumerableResult);
+        }
+
+    }
+}
--- MongoDBApp/ViewModels/MainViewModel.cs
+++ MongoDBApp/ViewModels/MainViewModel.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDBApp.Extensions;
 
 namespace MongoDBApp.ViewModels
 {
