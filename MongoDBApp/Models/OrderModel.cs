a2f70a41c0f8516d8fd2b6b8245cf3380b36acf3
BrianJVarley <brianvarley9@gmail.com>
2015-11-30 14:32:45 +0000

    Added orders and products POCO's

---
+++ MongoDBApp/Models/OrderModel.cs
@@ -0,0 +1,81 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDBApp.Models;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBApp.Models
+{
+    public class OrderModel : INotifyPropertyChanged
+    {
+
+        private ObjectId _id;
+        private string email;
+        private ProductModel[] products;
+
+
+        [BsonId]
+        public ObjectId Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+
+                _id = value;
+            }
+        }
+
+
+
+
+
+        [BsonElement("email")]
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                RaisePropertyChanged("Email");
+            }
+        }
+
+
+        [BsonElement("products")]
+        public ProductModel[] Products
+        {
+            get
+            {
+                return products;
+            }
+            set
+            {
+                products = value;
+                RaisePropertyChanged("Products");
+            }
+        }       
+        
+        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
---
+++ MongoDBApp/Models/ProductModel.cs
@@ -0,0 +1,74 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBApp.Models
+{
+    public class ProductModel : INotifyPropertyChanged
+    {
+
+        private string productId;
+        private float price;
+        private string description;
+
+
+        
+
+
+        [BsonElement("productId")]
+        public string ProductId
+        {
+            get
+            {
+                return productId;
+            }
+            set
+            {
+                productId = value;
+                RaisePropertyChanged("ProductId");
+            }
+        }
+
+        [BsonElement("price")]
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                RaisePropertyChanged("Price");
+            }
+        }
+
+        [BsonElement("description")]
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
