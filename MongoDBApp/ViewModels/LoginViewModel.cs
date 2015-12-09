3dbcdb76ef298ddf210e2c6b28981fb4937d59e3
BrianJVarley <brianvarley9@gmail.com>
2015-12-09 22:41:11 +0000

    Setting up Auth0, user access feature

---
+++ MongoDBApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Auth0.Windows;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBApp.ViewModels
+{
+    [ImplementPropertyChanged]
+    class LoginViewModel : IPageViewModel
+    {
+
+        public string Name {get;}
+        public bool IsEnabled { get; set; }
+
+        private Auth0Client auth0 = new Auth0Client(ConfigurationManager.AppSettings["auth0:Domain"],
+                                                    ConfigurationManager.AppSettings["auth0:ClientId"]);
+
+        public LoginViewModel()
+        {
+
+
+        }
+       
+    }
+}
